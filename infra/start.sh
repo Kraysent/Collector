304a8c433abf4c2dbb09b14fa5070ac2b4bc80c3
Artyom Zaporozhets <kraysent@gmail.com>
2023-08-05 13:11:20 +0300

    Use host network in docker containers

--- infra/start.sh
+++ infra/start.sh
@@ -1,14 +1,14 @@
 export REGISTRY_ID=crpgqd2rqkfgjq6c275u
 docker run \
-  -p 9100:9100 \
   --detach \
   --env-file .env \
+  --network host \
   cr.yandex/${REGISTRY_ID}/collector:latest
 docker run \
   --detach \
   -e AGENT_MODE=flow \
   -v ${PWD}/config.river:/etc/agent/config.river \
-  -p 12345:12345 \
   --env-file .env \
+  --network host \
   grafana/agent:latest \
     run --server.http.listen-addr=0.0.0.0:12345 /etc/agent/config.river
